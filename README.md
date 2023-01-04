879f83f0bc76feb5d3135cd782705887b7f1d61f
crides <zhuhaoqing@live.cn>
2023-01-04 17:14:08 -0600

    setup

---
+++ README.md
@@ -0,0 +1,5 @@
+# kicaq
+
+Utilities for interfacing with Kicad PCB from mainly cadquery, for automated 3D model creation parametric on PCB design
+
+Note: you need to have Kicad installed _and_ have the python module importable
---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+  
+with open("README.md", "r") as fh:
+    description = fh.read()
+  
+setuptools.setup(
+    name="kicaq",
+    version="0.0.1",
+    author="crides",
+    author_email="zhuhaoqing@live.cn",
+    packages=["kicaq"],
+    description="Utilities for interfacing with Kicad PCB from cadquery",
+    long_description=description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/crides/kicaq",
+    license='MIT',
+    python_requires='>=3.10',
+    install_requires=["cadquery>=2"]
+)
